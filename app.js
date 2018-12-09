5fc07582e754492e9002ae98434fea48c25d76b4
Orlee Gillis <orlee.gillis@gmail.com>
2018-12-09 15:17:43 +0200

    trying to complete checkpoint

--- app.js
+++ app.js
@@ -1,52 +1,38 @@
-function onReady(){
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
-
-  // takes a type and a listener which is a function
-  // ES6 arrow function == standard anonymous function
-  addToDoForm.addEventListener('submit', () => {
-    event.preventDefault();
-
-    // get the text
-    let title = newToDoText.value;
-
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    checkbox.type = "checkbox";
-
-    // create a ability to delete item
-    let minusBtn = document.createElement('button');
-    minusBtn.innerHTML = '<span>Delete</span>';;
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the checkbox to the li
-    newLi.appendChild(minusBtn);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    // empty the input value
+function onReady() {
+  const toDos = [];
+   const addToDoForm = document.getElementById('addToDoForm');
+  function createNewToDo() {
+ const newToDoText = document.getElementById('newToDoText');
+     if (!newToDoText.value) { return; }
+ toDos.push({
+  title: newToDoText.value,
+  complete: false
+});
     newToDoText.value = '';
-
-    // listening for minusBtn to be clicked and removing the item
-    minusBtn.addEventListener('click', function() {
-      newLi.parentNode.removeChild(newLi);
-    })
-
-    console.log(title);
+        renderTheUI();
+  }
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
   });
+    renderTheUI(); {
+ const toDoList = document.getElementById('toDoList');
 
-};
+ toDoList.textContent = '';
+
+ toDos.forEach(function(toDo) {
+   const newLi = document.createElement('li');
+   const checkbox = document.createElement('input');
+   checkbox.type = "checkbox";
+         newLi.textContent = toDo.title;
+         toDoList.appendChild(newLi);
+ newLi.appendChild(checkbox);
+   });
+  }
+
+
+}
+
 
 window.onload = function() {
   onReady();
